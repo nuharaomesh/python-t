3f222ca3f7500f000b45c52d4a6da6c1b0291011
nuharaomesh <omeshnuhara4@gmail.com>
2024-12-27 22:03:40 +0530

    mining filter function behaviors

---
+++ 4thDay/new_filter.py
@@ -0,0 +1,24 @@
+# number = [1, 2, 3, 4, 5]
+
+# def is_even(x):
+#     return x % 2 == 0
+
+# even_numbers = filter(is_even, number)
+# print(type(even_numbers))
+
+# person = [
+#     {
+#         "name": "Omesh",
+#         "age": 42
+#     },
+#     {
+#         "name": "Nuhara",
+#         "age": 32
+#     }
+# ]
+
+# def find_adult(person):
+#     return int(person["age"]) >= 18
+
+# adult_persons = filter(find_adult, person)
+# print(list(adult_persons))
